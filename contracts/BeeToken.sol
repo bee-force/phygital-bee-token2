b48f33bd4a649f814ceaf64d0dc5cb1133962907
Berta <berta.dautfest@hotmail.com>
2023-06-25 12:13:11 +0200

    clean up

--- contracts/BeeToken.sol
+++ contracts/BeeToken.sol
@@ -3,37 +3,39 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-//state variable only functions within contract can modify the state variable - requires gas to call the function 
-contract beeToken is ERC721URIStorage, Ownable {
-    uint public tokenCount;
-    // constructor function is called when contract is deployed onto blockchain, inherited contract erc721
-    constructor() ERC721("Phygital-BeeToken", "PBT"){}
-    // when minting we are passing the link to the metadata (ipfs in this case)
-    // when passing strings you need to specify the memory location of the argument & set visability of the function, is supposed to called from the outside: external keyword & return current tokencount + datatype
-    function mint(string memory _tokenURI) external returns(uint) {
-        // increase tokencount by 1 
-        tokenCount ++;
-        // mint a new nft, by calling the internal safe mint function, 1st argument the address that we're minting for - caller address
-        _safeMint(msg.sender, tokenCount);
-        // id of token we can get from token count 
-        _setTokenURI(tokenCount, _tokenURI);
-        return(tokenCount);
+
+contract BeeToken is ERC721URIStorage, Ownable {
+    uint public tokenCount; // Stores the total number of tokens minted
+
+    constructor() ERC721("Phygital-BeeToken", "PBT") {}
+
+    /**
+     * @dev Mint a new BeeToken NFT.
+     * @param _tokenURI The URI for the token's metadata (e.g., IPFS link).
+     * @return The token ID of the minted NFT.
+     */
+    function mint(string memory _tokenURI) external returns (uint) {
+        tokenCount++; // Increase the token count by 1
+        _safeMint(msg.sender, tokenCount); // Mint a new NFT for the caller's address
+        _setTokenURI(tokenCount, _tokenURI); // Set the token URI for the minted NFT
+        return tokenCount; // Return the token ID of the minted NFT
     }
 
-function withdraw() public payable onlyOwner {
-    // This will pay Berta of the initial sale.
-    // =============================================================================
-    (bool hs, ) = payable(0xA661cbcc7F0C4805B08501793D67ef668b8133A1).call{value: address(this).balance * 5 / 100}("");
-    require(hs);
-    // =============================================================================
-    
-    // This will payout the owner 95% of the contract balance.
-    // Do not remove this otherwise you will not be able to withdraw the funds.
-    // =============================================================================
-    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
-    require(os);
-    // =============================================================================
-  }
+    /**
+     * @dev Withdraw the contract's balance.
+     * This function is only accessible by the contract owner.
+     * The contract owner will receive 95% of the contract balance,
+     * and 5% of the contract balance will be sent to address 0xA661cbcc7F0C4805B08501793D67ef668b8133A1.
+     */
+    function withdraw() public payable onlyOwner {
+        // Pay 5% of the contract balance to address 0xA661cbcc7F0C4805B08501793D67ef668b8133A1
+        (bool hs, ) = payable(0xA661cbcc7F0C4805B08501793D67ef668b8133A1).call{value: address(this).balance * 5 / 100}("");
+        require(hs);
 
+        // Pay 95% of the contract balance to the contract owner
+        (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+        require(os);
+    }
 }
+
  
