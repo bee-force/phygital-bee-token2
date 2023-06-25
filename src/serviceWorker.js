d9477f7da45ad795d26a64f72d2e91d2a37fbb53
Berta <berta.dautfest@hotmail.com>
2023-06-25 13:19:38 +0200

    Update serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,4 +1,4 @@
-// This optional code is used to register a service worker.
+// This optional code is used to register a service worker. & not used in the project yet
 // register() is not called by default.
 
 // This lets the app load faster on subsequent visits in production, and gives
